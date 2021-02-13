aaf60b8cdba90a81308dd2588bc72934373f0a20
Monica <m_spiritas@mail.fhsu.edu>
2021-02-13 16:16:21 -0600

    typo

--- item_list.php
+++ item_list.php
@@ -1,7 +1,7 @@
 <?php
 require_once('database.php');
 
-// Get all categories
+
 $query = 'SELECT * FROM todoitems
                        ORDER BY ItemNum';
 $statement = $db->prepare($query);
@@ -12,13 +12,12 @@ $statement->closeCursor();
 <!DOCTYPE html>
 <html>
 
-<!-- the head section -->
+
 <head>
     <title>ToDoList</title>
     <link rel="stylesheet" type="text/css" href="css/main.css" />
 </head>
 
-<!-- the body section -->
 <body>
 <header><h1>ToDo List</h1></header>
 <main>
@@ -29,7 +28,7 @@ $statement->closeCursor();
             <th>&nbsp;</th>
         </tr>
 
-        <!-- The rest of the table -->
+        
         <tr>
         <?php foreach ($items as $item) : ?>
             <td><?php echo $item['Title']; ?></td>
@@ -45,7 +44,6 @@ $statement->closeCursor();
 
     <h2>Add Category</h2>
     
-    <!-- add product form -->
     <form action="add_item.php" method="post"
             id="add_item_form">
         <label>Name:</label>
